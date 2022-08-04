13ce0a9d1e239ad8fecf667f7b0957d3d2a4a346
Asen Malinov <m4linov@gmail.com>
2022-08-04 22:25:57 +0300

    Change navigation links for different users

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,62 +1,35 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
-
+import AuthContext from "../../contexts/AuthContext";
+import './Navigation.css';
 const Navigation = () => {
+
+    const { user } = useContext(AuthContext);
+
     return (
         <nav
             className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light"
             id="ftco-navbar"
         >
             <div className="container">
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#ftco-nav"
-                    aria-controls="ftco-nav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="fa fa-bars" /> Menu
-                </button>
                 <div className="collapse navbar-collapse" id="ftco-nav">
                     <ul className="navbar-nav mr-auto">
-                        <li className="nav-item" >
-                            <Link to="/" className="nav-link btn-custom">
-                                Home
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/services" className="nav-link btn-custom">
-                                Services
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/login" className="nav-link">
-                                Login
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/register" className="nav-link">
-                                Register
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/logout" className="nav-link">
-                                Logout
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            {/* TODO: use user's ID for the correct link !!! */}
-                            <Link to="/my-sevices" className="nav-link">
-                                My Services
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            {/* TODO: see if you need this link !!! */}
-                            <Link to="/about-us" className="nav-link">
-                                About Us
-                            </Link>
-                        </li>
+                        <li className="nav-item"><Link to="/" className="nav-item nav-link btn-custom" title="Home">Home</Link></li>
+                        {!user.email ?
+                            <>
+                                <li className="nav-item"><Link to="/login" className="nav-link"> Login </Link></li>
+                                <li className="nav-item"><Link to="/register" className="nav-link"> Register </Link></li>
+                                <li className="nav-item"><Link to="/about-us" className="nav-link"> About Us </Link></li>
+                            </>
+                            :
+                            <>
+                                <li className="nav-item"><Link to="/add-car" className="nav-link btn-custom" title="Services"> Add Car </Link></li>
+                                <li className="nav-item"><Link to="/services" className="nav-link btn-custom" title="Services"> Services </Link></li>
+                                <li className="nav-item"><Link to="/my-sevices" className="nav-link"> My Services </Link></li>
+                                <li className="nav-item"><Link to="/logout" className="nav-link logout">  Logout </Link></li>
+                                <li className="nav-item"><Link to="/profile" className="nav-link profile">Profile {user.email}</Link></li>
+                            </>
+                        }
                     </ul>
                 </div>
             </div>
