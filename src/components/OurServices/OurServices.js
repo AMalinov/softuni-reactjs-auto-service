a6af57e21a8e86dbbaca7dc7909b0b182f1c5b92
Asen Malinov <m4linov@gmail.com>
2022-08-04 22:47:52 +0300

    Changed heading

--- src/components/OurServices/OurServices.js
+++ src/components/OurServices/OurServices.js
@@ -1,118 +1,122 @@
 import "./OurServices.css";
-
+import {Helmet} from "react-helmet";
 const OurServices = () => {
     return (
-        <div className="autho-services">
-            <section className="ftco-section">
-                <div className="container">
-                    <div className="row justify-content-center pb-5 mb-3">
-                        <div className="col-md-7 heading-section text-center ftco-animate fadeInUp ftco-animated">
-                            <span className="subheading">Ние предлагаме услуги</span>
-                            <h2>Нашите автомобилни услуги</h2>
+        <>
+        <Helmet>
+            <title>AutoService | Services</title>
+        </Helmet>
+            <div className="autho-services">
+                <section className="ftco-section">
+                    <div className="container">
+                        <div className="row justify-content-center pb-5 mb-3">
+                            <div className="col-md-7 heading-section text-center ftco-animate fadeInUp ftco-animated">
+                                <h2>Our Services</h2>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-4 services ftco-animate fadeInUp ftco-animated">
+                                <div className="d-block d-flex">
+                                    <div className="icon d-flex justify-content-center align-items-center">
+                                        <span className="flaticon-car-service" />
+                                    </div>
+                                    <div className="media-body pl-3">
+                                        <h3 className="heading">Oil change</h3>
+                                        <p>
+                                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, eius?
+                                        </p>
+                                        <p>
+                                            <button className="btn-my-custom">
+                                                Order Service
+                                            </button>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="d-block d-flex">
+                                    <div className="icon d-flex justify-content-center align-items-center">
+                                        <span className="flaticon-tyre" />
+                                    </div>
+                                    <div className="media-body pl-3">
+                                        <h3 className="heading">Tire change</h3>
+                                        <p>
+                                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas, aperiam!
+                                        </p>
+                                        <p>
+                                            <button className="btn-my-custom">
+                                                Order Service
+                                            </button>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 services ftco-animate fadeInUp ftco-animated">
+                                <div className="d-block d-flex">
+                                    <div className="icon d-flex justify-content-center align-items-center">
+                                        <span className="flaticon-battery" />
+                                    </div>
+                                    <div className="media-body pl-3">
+                                        <h3 className="heading">Batery</h3>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, nostrum.</p>
+                                        <p>
+                                            <button className="btn-my-custom">
+                                                Order Service
+                                            </button>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="d-block d-flex">
+                                    <div className="icon d-flex justify-content-center align-items-center">
+                                        <span className="flaticon-car-engine" />
+                                    </div>
+                                    <div className="media-body pl-3">
+                                        <h3 className="heading">Engine repair</h3>
+                                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, soluta.</p>
+                                        <p>
+                                            <button className="btn-my-custom">
+                                                Order Service
+                                            </button>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 services ftco-animate fadeInUp ftco-animated">
+                                <div className="d-block d-flex">
+                                    <div className="icon d-flex justify-content-center align-items-center">
+                                        <span className="flaticon-tow-truck" />
+                                    </div>
+                                    <div className="media-body pl-3">
+                                        <h3 className="heading">Emergency car</h3>
+                                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque, nisi.
+                                        </p>
+                                        <p>
+                                            <button className="btn-my-custom">
+                                                Order Service
+                                            </button>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="d-block d-flex">
+                                    <div className="icon d-flex justify-content-center align-items-center">
+                                        <span className="flaticon-repair" />
+                                    </div>
+                                    <div className="media-body pl-3">
+                                        <h3 className="heading">Car maintenance</h3>
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, pariatur.
+                                        </p>
+                                        <p>
+                                            <button className="btn-my-custom">
+                                                Order Service
+                                            </button>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col-md-4 services ftco-animate fadeInUp ftco-animated">
-                            <div className="d-block d-flex">
-                                <div className="icon d-flex justify-content-center align-items-center">
-                                    <span className="flaticon-car-service" />
-                                </div>
-                                <div className="media-body pl-3">
-                                    <h3 className="heading">Oil change</h3>
-                                    <p>
-                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, eius?
-                                    </p>
-                                    <p>
-                                        <button className="btn-my-custom">
-                                            Order Service
-                                        </button>
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="d-block d-flex">
-                                <div className="icon d-flex justify-content-center align-items-center">
-                                    <span className="flaticon-tyre" />
-                                </div>
-                                <div className="media-body pl-3">
-                                    <h3 className="heading">Tire change</h3>
-                                    <p>
-                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas, aperiam!
-                                    </p>
-                                    <p>
-                                        <button className="btn-my-custom">
-                                            Order Service
-                                        </button>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4 services ftco-animate fadeInUp ftco-animated">
-                            <div className="d-block d-flex">
-                                <div className="icon d-flex justify-content-center align-items-center">
-                                    <span className="flaticon-battery" />
-                                </div>
-                                <div className="media-body pl-3">
-                                    <h3 className="heading">Batery</h3>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, nostrum.</p>
-                                    <p>
-                                        <button className="btn-my-custom">
-                                            Order Service
-                                        </button>
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="d-block d-flex">
-                                <div className="icon d-flex justify-content-center align-items-center">
-                                    <span className="flaticon-car-engine" />
-                                </div>
-                                <div className="media-body pl-3">
-                                    <h3 className="heading">Engine repair</h3>
-                                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, soluta.</p>
-                                    <p>
-                                        <button className="btn-my-custom">
-                                            Order Service
-                                        </button>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4 services ftco-animate fadeInUp ftco-animated">
-                            <div className="d-block d-flex">
-                                <div className="icon d-flex justify-content-center align-items-center">
-                                    <span className="flaticon-tow-truck" />
-                                </div>
-                                <div className="media-body pl-3">
-                                    <h3 className="heading">Emergency car</h3>
-                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque, nisi.
-                                    </p>
-                                    <p>
-                                        <button className="btn-my-custom">
-                                            Order Service
-                                        </button>
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="d-block d-flex">
-                                <div className="icon d-flex justify-content-center align-items-center">
-                                    <span className="flaticon-repair" />
-                                </div>
-                                <div className="media-body pl-3">
-                                    <h3 className="heading">Car maintenance</h3>
-                                    <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, pariatur.
-                                    </p>
-                                    <p>
-                                        <button className="btn-my-custom">
-                                            Order Service
-                                        </button>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
+                </section>
+            </div>
+        </>
     );
 }
 
