994bf047cf67db44d5f9bc596ac1e4d0a4c26650
Asen Malinov <m4linov@gmail.com>
2022-08-04 22:26:31 +0300

    Changed links in home view for different users

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,19 @@
 import { Link } from "react-router-dom";
 import "./Home.css"
+import { Helmet } from "react-helmet";
+import AuthContext from "../../contexts/AuthContext";
+import { useContext } from "react";
 
 const Home = () => {
+
+    const { user } = useContext(AuthContext);
+
     return (
 
         <>
+            <Helmet>
+                <title>AutoService | Home</title>
+            </Helmet>
             <div className="bg"></div>
             <section className="intro">
                 <div className="container intro-wrap">
@@ -16,8 +25,13 @@ const Home = () => {
                                 </div>
                                 <h2 className="mb-0">Don't let you'r car become this! <span>We can repair it for you!</span></h2>
                             </div>
-                            <Link to="/services" className="bg-primary btn-custom d-flex align-items-center"><span>Check our services</span></Link>
-                            <Link to="/comments" className="bg-primary btn-custom d-flex align-items-center"><span>Hear from our costumers</span></Link>
+
+                            {user.email
+                                ?
+                                <Link to="/services" className="bg-primary btn-custom d-flex align-items-center"><span>Check our services</span></Link>
+                                :
+                                <Link to="/comments" className="bg-primary btn-custom d-flex align-items-center"><span>Hear from our costumers</span></Link>
+                            }
                         </div>
                     </div>
                 </div>
