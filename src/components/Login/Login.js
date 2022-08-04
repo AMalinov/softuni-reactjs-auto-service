880145aba02d53c48d483f3fc997ee3e6c519b15
Asen Malinov <m4linov@gmail.com>
2022-08-04 22:46:12 +0300

    Added Register, Login and Logout components

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,60 +1,103 @@
 import { Link } from "react-router-dom";
 import "./Login.css"
+
+import { Helmet } from 'react-helmet';
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../../contexts/AuthContext.js";
+import * as authService from "../../services/authService";
+
+
 const Login = () => {
+
+    const { login } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const loginHandler = (e) => {
+        e.preventDefault();
+
+        let formData = new FormData(e.currentTarget);
+
+        let email = formData.get("email");
+        let password = formData.get("password");
+
+        authService
+            .login(email, password)
+            .then((authData) => {
+                login(authData);
+
+                navigate("/");
+            })
+            .catch((err) => {
+                alert(err);
+            });
+    };
+
+
     return (
-        <section className="vh-100 gradient-custom">
-            <div className="container py-5 h-100">
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                    <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                        <div
-                            className="card bg-dark text-white"
-                        >
-                            <div className="card-body p-5 text-center">
-                                <div className="mb-md-5 mt-md-4 pb-5">
-                                    <h2 className="fw-bold mb-2 text-uppercase"
-                                    >Login</h2>
-                                    <div className="form-outline form-white mb-4">
-                                        <input
-                                            type="email"
-                                            id="typeEmailX"
-                                            className="form-control form-control-lg"
-                                        />
-                                        <label className="form-label" htmlFor="typeEmailX">
-                                            Email
-                                        </label>
+        <>
+            <Helmet>
+                <title>AutoService | Login</title>
+            </Helmet>
+            <section className="vh-100 gradient-custom">
+                <div className="container py-5 h-100">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                            <div
+                                className="card bg-dark text-white"
+                            >
+                                <form className="loginForm" onSubmit={loginHandler}>
+                                    <div className="card-body p-5 text-center">
+                                        <div className="mb-md-5 mt-md-4 pb-5">
+                                            <h2 className="fw-bold mb-2 text-uppercase"
+                                            >Login</h2>
+                                            <div className="form-outline form-white mb-4">
+                                                <input
+                                                    type="email"
+                                                    id="email"
+                                                    name="email"
+                                                    placeholder="email@example.com"
+                                                    className="form-control form-control-lg"
+                                                />
+                                                <label className="form-label" htmlFor="email">
+                                                    Email
+                                                </label>
+                                            </div>
+                                            <div className="form-outline form-white mb-4">
+                                                <input
+                                                    type="password"
+                                                    id="password"
+                                                    name="password"
+                                                    placeholder="**********"
+                                                    className="form-control form-control-lg"
+                                                />
+                                                <label className="form-label" htmlFor="typePasswordX">
+                                                    Password
+                                                </label>
+                                            </div>
+                                            <button
+                                                className="btn btn-outline-light btn-lg px-5"
+                                                type="submit"
+                                            >
+                                                Login
+                                            </button>
+                                        </div>
+                                        <div className="bottom">
+                                            <p className="mb-0">
+                                                <span>Don't have an account?</span>
+                                                <Link to="/register" className="text-white-sign-up fw-bold">
+                                                    Sign Up
+                                                </Link>
+                                            </p>
+                                        </div>
                                     </div>
-                                    <div className="form-outline form-white mb-4">
-                                        <input
-                                            type="password"
-                                            id="typePasswordX"
-                                            className="form-control form-control-lg"
-                                        />
-                                        <label className="form-label" htmlFor="typePasswordX">
-                                            Password
-                                        </label>
-                                    </div>
-                                    <button
-                                        className="btn btn-outline-light btn-lg px-5"
-                                        type="submit"
-                                    >
-                                        Login
-                                    </button>
-                                </div>
-                                <div className="bottom">
-                                    <p className="mb-0">
-                                        <span>Don't have an account?</span>
-                                        <Link to="/register" className="text-white-sign-up fw-bold">
-                                            Sign Up
-                                        </Link>
-                                    </p>
-                                </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-
+            </section>
+        </>
     );
 };
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,69 +1,122 @@
 import { Link } from "react-router-dom";
 import './Register.css';
+
+import { Helmet } from "react-helmet";
+import { useContext, useRef } from "react";
+import { useNavigate } from "react-router";
+
+import * as authService from "../../services/authService";
+import AuthContext from "../../contexts/AuthContext";
+
+
 const Register = () => {
+
+    const navigate = useNavigate();
+    const { login } = useContext(AuthContext);
+
+    const registerHandler = (e) => {
+        e.preventDefault();
+        // console.log(e.target.email.value);
+        let { email, password} = Object.fromEntries(
+            new FormData(e.currentTarget)
+        );
+
+        authService.register(email, password).then((authData) => {
+            login(authData);
+
+            navigate("/");
+        });
+    };
+
+    const textInput = useRef(null);
+
+    const rePassValidation = (e) => {
+        e.preventDefault();
+        let rePassword = e.target.value;
+        let password = textInput.current.value;
+        if (password !== rePassword) {
+            alert("Passwords must match");
+        }
+    };
+
+
+
+
     return (
-        <section className="vh-100 gradient-custom">
-            <div className="container py-5 h-100">
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                    <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                        <div
-                            className="card bg-dark text-white"
-                        >
-                            <div className="card-body p-5 text-center">
-                                <div className="mb-md-5 mt-md-4 pb-5">
-                                    <h2 className="fw-bold mb-2 text-uppercase"
-                                    >Register</h2>
-                                    <div className="form-outline form-white mb-4">
-                                        <input
-                                            type="email"
-                                            id="typeEmailX"
-                                            className="form-control form-control-lg"
-                                        />
-                                        <label className="form-label" htmlFor="typeEmailX">
-                                            Email
-                                        </label>
+        <>
+        <Helmet>
+            <title>AutoService | Register</title>
+        </Helmet>
+            <section className="vh-100 gradient-custom">
+                <div className="container py-5 h-100">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                            <div className="card bg-dark text-white">
+                                <form className="registerForm" onSubmit={registerHandler}>
+                                    <div className="card-body p-5 text-center">
+                                        <div className="mb-md-5 mt-md-4 pb-5">
+                                            <h2 className="fw-bold mb-2 text-uppercase" >Register</h2>
+                                            <div className="form-outline form-white mb-4">
+                                                <input
+                                                    type="text"
+                                                    id="email"
+                                                    name="email"
+                                                    placeholder="email@example.com"
+                                                    className="form-control form-control-lg"
+                                                />
+                                                <label className="form-label" htmlFor="email">
+                                                    Email
+                                                </label>
+                                            </div>
+                                            <div className="form-outline form-white mb-4">
+                                                <input
+                                                    type="password"
+                                                    id="password"
+                                                    name="password"
+                                                    placeholder="**********"
+                                                    className="form-control form-control-lg"
+                                                />
+                                                <label className="form-label" htmlFor="password">
+                                                    Password
+                                                </label>
+                                            </div>
+                                            <div className="form-outline form-white mb-4">
+                                                <input
+                                                    type="password"
+                                                    id="rePassword"
+                                                    name="rePassword"
+                                                    placeholder="**********"
+                                                    onBlur={rePassValidation}
+                                                    className="form-control form-control-lg"
+                                                />
+                                                <label className="form-label" htmlFor="rePassword">
+                                                    Confirm Password
+                                                </label>
+                                            </div>
+                                            <button
+                                                className="btn btn-outline-light btn-lg px-5"
+                                                type="submit"
+                                                
+                                            >
+                                                Register
+                                            </button>
+                                        </div>
+                                        <div className="bottom">
+                                            <p className="mb-0">
+                                                <span>Already have an account?</span>
+                                                <Link to="/login" className="text-white-sign-up fw-bold">
+                                                    Login
+                                                </Link>
+                                            </p>
+                                        </div>
                                     </div>
-                                    <div className="form-outline form-white mb-4">
-                                        <input
-                                            type="password"
-                                            id="typePasswordX"
-                                            className="form-control form-control-lg"
-                                        />
-                                        <label className="form-label" htmlFor="typePasswordX">
-                                            Password
-                                        </label>
-                                    </div>
-                                    <div className="form-outline form-white mb-4">
-                                        <input
-                                            type="password"
-                                            id="typePasswordX"
-                                            className="form-control form-control-lg"
-                                        />
-                                        <label className="form-label" htmlFor="typePasswordX">
-                                            Confirm Password
-                                        </label>
-                                    </div>
-                                    <button
-                                        className="btn btn-outline-light btn-lg px-5"
-                                        type="submit"
-                                    >
-                                        Register
-                                    </button>
-                                </div>
-                                <div className="bottom">
-                                    <p className="mb-0">
-                                        <span>Already have an account?</span>
-                                        <Link to="/login" className="text-white-sign-up fw-bold">
-                                            Login
-                                        </Link>
-                                    </p>
-                                </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     );
 }
 
