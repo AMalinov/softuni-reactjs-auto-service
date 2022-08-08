524c4a53bce0b7b1bc0cb96f59fc9142e7e60673
Asen Malinov <m4linov@gmail.com>
2022-08-08 12:43:20 +0300

    Styled user profile

--- src/components/Profile/MyCars/MyCars.js
+++ src/components/Profile/MyCars/MyCars.js
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
+import './MyCars.css';
 const MyCars = ({
     car
 }) => {
+
+    const editHandler = (e) => {
+
+    }
+
+
     return (
         <div className="myCars">
             <div className="myCars-info">
+                <img className="carImage" src={car.imageURL} alt={car.carBrand + ' ' + 'car image'} />
                 <ul key={car._id}>
-                    <img src={car.imageURL} />
+                    <li>{car.regNumber}</li>
                     <li>{car.carBrand}</li>
                     <li>{car.year}</li>
                     <li>{car.carMilage}</li>
                     <li>{car.description}</li>
                     <li>{car.ownerEmail}</li>
+                    <button className="btn btn-outline-light btn-lg px-5 edit" onClick={editHandler}>Edit</button>
+                    <button className="btn btn-outline-light btn-lg px-5 remove">Remove</button>
                 </ul>
-                <button>Edit</button>
-                <button>Remove</button>
             </div>
         </div>
     );
