545c9d818cf8d69176bcf56b227503532300f387
Asen Malinov <m4linov@gmail.com>
2022-08-12 20:34:25 +0300

    Fixed catalog rendering

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,14 +1,24 @@
+import { useEffect, useState } from 'react';
 import './Catalog.css';
 import CatalogItem from './CatalogItem/CatalogItem';
+import * as carService from '../../services/carService';
 
 const Catalog = ({ cars }) => {
+    const [allCars, setAllCars] = useState([]);
+
+    useEffect(() => {
+        carService.getAllCars()
+            .then(result => {
+                setAllCars(result);
+            });
+    }, []);
 
     return (
         <section id="catalog-page">
             <h1>All Cars in service</h1>
 
-            {cars.length > 0
-                ? cars.map(x => <CatalogItem key={x._id} car={x} />)
+            {allCars.length > 0
+                ? allCars.map(x => <CatalogItem key={x._id} car={x} />)
                 : <h3 className="no-cars">Everithing is fixed for now. No cars in service.</h3>
             }
         </section>
