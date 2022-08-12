3f844ad774060b3e0e6e309f4f5aa501e5b6a20d
Asen Malinov <m4linov@gmail.com>
2022-08-12 18:00:14 +0300

    Fixed links and styling

--- src/components/Profile/MyCars/MyCarDetails/MyCarDetails.js
+++ src/components/Profile/MyCars/MyCarDetails/MyCarDetails.js
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import * as carService from '../../../../services/carService';
+import './MyCarDetails.css';
+
 const MyCarDetails = (car) => {
+
+    const [showDialogue, setShowDialogue] = useState(false);
 
     const [currentCar, setCurrentCar] = useState({});
     const { carId } = useParams();
@@ -11,23 +15,26 @@ const MyCarDetails = (car) => {
             .then(carData => {
                 setCurrentCar(carData);
             })
-    }, []);
+    }, [carId]);
 
+    console.log('likes -> ' + currentCar.likes);
+    console.log(currentCar.usedServices);
     return (
-        <div className="myCars">
-        <div className="myCars-info">
-            <img className="carImage" src={currentCar.imageURL} alt={currentCar.carBrand + ' ' + 'car image'} />
-            <ul key={currentCar._id}>
-                <li>Brand : {currentCar.carBrand}</li>
-                <li>Registration number : {currentCar.regNumber}</li>
-                <li>Manufacture Year : {currentCar.year}</li>
-                <li>Car Milage : {currentCar.carMilage}</li>
-                <li>Problem Description : {currentCar.description}</li>
-                <Link to={`/profile/${currentCar._id}/edit`} className="btn btn-outline-light btn-lg px-5 edit">Edit</Link>
-                <Link to={`/profile/${currentCar._id}/delete`} className="btn btn-outline-light btn-lg px-5 remove">Remove</Link>
-            </ul>
+        <div className="myCars-detailsCar">
+            <h2 className="details-h2">Details for your {currentCar.carBrand} : {currentCar.regNumber}</h2>
+            <div className="myCars-info">
+                <img className="carImage" src={currentCar.imageURL} alt={currentCar.carBrand + ' ' + 'car image'} />
+                <ul key={currentCar._id}>
+                    <li>Brand : {currentCar.carBrand}</li>
+                    <li>Registration number : {currentCar.regNumber}</li>
+                    <li>Manufacture Year : {currentCar.year}</li>
+                    <li>Car Milage : {currentCar.carMilage}</li>
+                    <li>Problem Description : {currentCar.description}</li>
+                    <Link to={`/profile/${currentCar._id}/edit`} className="btn btn-outline-light btn-lg px-5 edit">Edit</Link>
+                    <Link to={`/profile/${currentCar._id}/delete`} className="btn btn-outline-light btn-lg px-5 remove">Remove</Link>
+                </ul>
+            </div>
         </div>
-    </div>
     );
 }
 
